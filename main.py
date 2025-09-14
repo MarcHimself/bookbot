06284ae4a0b8017cc1f50b7f9e9a3f1735726760
Marc <girard1337@gmail.com>
2025-09-13 23:43:21 -0400

    bookbot done

--- main.py
+++ main.py
@@ -1,17 +1,20 @@
-from stats import get_num_words, get_char_count
+from stats import *
+import sys
 
 def main():
-    book_path = "./books/frankenstein.txt"
+    if len(sys.argv) < 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    book_path = sys.argv[1]
     text = get_book_text(book_path) 
     num_words = get_num_words(text)
     char_dict = get_char_count(text)
-    print(f"{num_words} words found in the document")
-    print(f"{char_dict}")
+    sorted_char_dict = sort_dictionnary(char_dict)
+    print_report(num_words, sorted_char_dict, book_path)
     
 def get_book_text(path):
     with open(path) as f:
         file_content = f.read()
         return file_content
-
 
 main()
--- stats.py
+++ stats.py
@@ -4,16 +4,30 @@ def get_num_words(text):
 
 def get_char_count(text):
     text = text.lower()
-    char_count = {
-        "t":0,
-        "p":0,
-        "c":0
-    }
+    # empty dict
+    char_count = {}
     for char in text:
-        if char == "t":
-            char_count["t"] += 1
-        elif char == "p":
-            char_count["p"] += 1
-        elif char == "c":
-            char_count["c"] += 1
+        # check if the character is a letter
+        if char.isalpha():
+            # if its already part of the dictionnary add 1    
+            if char in char_count: 
+                char_count[char] += 1
+            # if its not part of the dictionnary set it to 1
+            elif char not in char_count:
+                char_count[char] = 1
     return char_count
+
+def sort_dictionnary(unsorted_dict):
+    sorted_list = sorted(unsorted_dict.items(), key=lambda item: item[1], reverse=True)
+    sorted_dict = dict(sorted_list)
+    return sorted_dict
+
+def print_report(total_words, sorted_dict, book_path):
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {book_path}...")
+    print("----------- Word Count ----------")
+    print(f"Found {total_words} total words")
+    print("--------- Character Count -------")
+    for key, value in sorted_dict.items():
+        print(f"{key}: {value}")
+    print("============= END ===============")
